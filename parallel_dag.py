65320177d3227a07f184cc42b63de6781903599c
web3devdex <34705561+web3devdex@users.noreply.github.com>
2025-07-20 06:52:46 +0700

    Update parallel_dag.py

--- parallel_dag.py
+++ parallel_dag.py
@@ -6,18 +6,22 @@ from time import sleep
 def parallel_dag():
     @task
     def task_1():
+        print("task_1")
         sleep(30)
 
     @task
     def task_2():
+        print("task_2")
         sleep(30)
 
     @task
     def task_3():
+        print("task_3")
         sleep(30)
         
     @task
     def task_4():
+        print("task_4")
         print('Done')
 
     chain([task_1(), task_2(), task_3()], task_4())
