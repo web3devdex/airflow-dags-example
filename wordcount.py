a61542533ee2fbac3730165c28c2427f9a151c03
web3devdex <34705561+web3devdex@users.noreply.github.com>
2025-07-22 15:45:08 +0700

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,13 @@
 from pyspark.sql import SparkSession
 import sys
+import logging
+
+# Cấu hình logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     input_path = sys.argv[1] if len(sys.argv) > 1 else "/opt/airflow/dags/repo/input.txt"
@@ -15,6 +23,9 @@ if __name__ == "__main__":
               .flatMap(lambda line: line[0].split(" "))
               .map(lambda word: (word, 1))
               .reduceByKey(lambda a, b: a + b))
+    print("printing count")
+    for word, cnt in counts.take(10):
+        logger.info(f"{word}: {cnt}")
 
     # Lưu kết quả
     counts.saveAsTextFile(output_path)
