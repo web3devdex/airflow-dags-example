eb26d76ad285b110ad06bd58b3647e802396a5b3
web3devdex <34705561+web3devdex@users.noreply.github.com>
2025-07-22 14:48:07 +0700

    Create spark_example_dag.py

---
+++ spark_example_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='spark_wordcount_dag',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+
+    run_spark_job = BashOperator(
+        task_id='run_spark_wordcount',
+        bash_command=(
+            'spark-submit '
+            '--master local[2] '
+            '/opt/airflow/wordcount.py '
+            '/opt/airflow/input.txt '
+            '/opt/airflow/output_{{ ds }}'
+        )
+    )
+
+    run_spark_job
