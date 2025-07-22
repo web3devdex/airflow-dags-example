3bd714f05524caecd39a2fb8e18bfd38bc1efadd
web3devdex <34705561+web3devdex@users.noreply.github.com>
2025-07-22 16:02:30 +0700

    Update spark_example_dag.py

--- spark_example_dag.py
+++ spark_example_dag.py
@@ -1,28 +1,28 @@
-VERSION_CODE = "08-00-00-22-07-2025"
-
-from airflow.decorators import dag, task
-from airflow.models.baseoperator import chain
+from airflow.decorators import dag
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
-import subprocess
 
 @dag(
     dag_id="spark_wordcount_dag",
-    schedule="@daily",       # tương đương schedule_interval='@daily'
+    schedule="@daily",
     start_date=datetime(2025, 1, 1),
     catchup=False,
     tags=["spark", "pyspark"],
 )
 def spark_wordcount_dag():
-    @task
-    def run_spark_wordcount(execution_date: str = "{{ ds }}"):
-        """
-        Gọi spark-submit với PySpark job
-        """
-        bash_cmd = (f"spark-submit /opt/airflow/dags/repo/wordcount.py")
-        print(f"Running: {bash_cmd}")
-        subprocess.run(bash_cmd, shell=True, check=True)
+    spark_wordcount = SparkSubmitOperator(
+        task_id="spark_wordcount_task",
+        application="/opt/airflow/dags/repo/wordcount.py",
+        conn_id="spark_default",
+        application_args=[
+        ],
+        executor_cores=2,
+        executor_memory="1g",
+        driver_memory="1g",
+        verbose=True,
+    )
 
-    # Gọi task
-    run_spark_wordcount()
+    spark_wordcount  # chỉ có 1 task
 
+# Gọi DAG
 spark_wordcount_dag()
